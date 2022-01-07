d6aa86a3e7ea8555fd8bb4a9d7b0c48cd3edff68
joshua-poon <86510885+joshua-poon@users.noreply.github.com>
2022-01-07 12:22:38 +1100

    psl

--- conjugation_script.js
+++ conjugation_script.js
@@ -2,7 +2,7 @@
 
 //verb irregular conjugations
 
-const irrVerbConj = { 'être': ['to be', 'suis', 'es', 'est', 'sommes', 'êtes', 'sont', 'été'], 'avoir': ['to have', 'ai', 'as', 'a', 'avons', 'avez', 'ont', 'eu'], 'aller': ['to go', 'vais', 'vas', 'va', 'allons', 'allez', 'vont', 'allé(e) (with être)'], 'faire': ['to do, make', 'fais', 'fais', 'fait', 'faisons', 'faites', 'font', 'fait'],'vouloir':['to want, wish','veux','veux','veut','voulons','voulez','veulent','voulu' ]}
+const irrVerbConj = { 'être': ['to be', 'suis', 'es', 'est', 'sommes', 'êtes', 'sont', 'été'], 'avoir': ['to have', 'ai', 'as', 'a', 'avons', 'avez', 'ont', 'eu'], 'aller': ['to go', 'vais', 'vas', 'va', 'allons', 'allez', 'vont', 'allé(e) (with être)'], 'faire': ['to do, make', 'fais', 'fais', 'fait', 'faisons', 'faites', 'font', 'fait'], 'vouloir': ['to want, wish', 'veux', 'veux', 'veut', 'voulons', 'voulez', 'veulent', 'voulu'] }
 
 const pronounSelectors = ['.irr-eng', '.irr-je', '.irr-tu', '.irr-il', '.irr-nous', '.irr-vous', '.irr-ils', '.irr-pc']
 
@@ -13,7 +13,7 @@ for (let i = 0; i < irrVerbs.length; i++) {
         const currentWord = irrVerbs[i].textContent;
         const currentConjList = irrVerbConj[currentWord]
 
-        for (let j = 0; j < 8; j++)
+        for (let j = 0; j < pronounSelectors.length; j++)
             document.querySelector(pronounSelectors[j]).textContent = currentConjList[j]
 
 
